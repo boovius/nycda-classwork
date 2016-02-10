152016a49b763cb47c2a3e242ca1a50f66254b25
Joshua Book <joshua.book@gmail.com>
2016-02-10 15:42:34 -0500

    some keypress stuff

--- jquery-dom-manipulation/main.js
+++ jquery-dom-manipulation/main.js
@@ -1,9 +1,30 @@
 $(document).ready(function(){
-  // var element = $('h1');
-  // $('form').append(element);
-  $('form').submit(function(e){
-    console.log(e);
-    e.preventDefault();
-    $('header').append('<h3>this is the new header</h3>');
+
+  // $('form').submit(function(e){
+  //   console.log(e);
+  //   e.preventDefault();
+  //   $('header').append('<h3>this is the new header</h3>');
+  // });
+
+// REQUIREMENTS
+// when password gets above 7 change to green something
+
+// on every key press....
+// access the password field value
+// count num of chars in password field
+// if value.length > 7 turn green
+
+  $('#password').keypress(function(e){
+    if (e.currentTarget.value.length >= 7) {
+      $('#password').css('border', '1px solid green').blur();
+    }
   });
+
+  // $('#password').change(function(event){
+  //   if (event.currentTarget.value.length >= 7){
+  //     alert('awesome password');
+  //   } else {
+  //     alert('try again loserrrrrrr');
+  //   }
+  // });
 });
