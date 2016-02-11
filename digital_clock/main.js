87ff31cf2112859ed2186a30d12fc6bbb19478bd
Joshua Book <joshua.book@gmail.com>
2016-02-11 11:12:47 -0500

    clock with odd/even minute logic

--- digital_clock/main.js
+++ digital_clock/main.js
@@ -1,10 +1,42 @@
 $(document).ready(function(){
   function displayTime(){
-    var x = new Date;
-    // $('#hours').text(x.getHours());
-    // $('#minutes').text(x.getMinutes());
-    // $('#seconds').text(x.getSeconds());
-    $('#thetime').text(x.toLocaleString());
+    var date = new Date;
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    var milliseconds = date.getMilliseconds();
+
+    $('#hours').text(hours);
+    $('#minutes').text(formatMinutesAndSeconds(minutes));
+    $('#seconds').text(formatMinutesAndSeconds(seconds));
+    $('#milliseconds').text(milliseconds);
+
+    ifMinOdd(minutes);
   }
-  window.setInterval(displayTime, 1000);
+
+  function formatMinutesAndSeconds(value) {
+    if (value < 10) {
+      return '0'+ value;
+    } else {
+      return value;
+    }
+  }
+
+  function ifMinOdd(min){
+    if (min % 2 === 0) {
+      $('#minutes').css('color', 'red');
+    }
+  }
+
+  window.setInterval(displayTime, 1);
 });
+
+
+// get hours, minutes, seconds, milliseconds
+// display them on the page
+
+// determine if min is odd/even
+// if odd
+  // change the style to be black
+// else
+  // change the style to another color
