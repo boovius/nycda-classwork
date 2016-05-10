55ff287da2b2d426a69aa77f695c2765dcb2a078
Joshua Book <joshua.book@gmail.com>
2016-05-09 20:44:13 -0400

    made a second input and button to display value on click

--- web-dev-100/jQuery functions & events/index.js
+++ web-dev-100/jQuery functions & events/index.js
@@ -1,16 +1,14 @@
 $(document).ready(function(){
 
-  // respond to a click of button-1
   var button = $('#button-1');
-
-  
   button.click(function(){
-    // grab the value of input
     var value = $('#field-1').val()
-    // populate display with value
     $('#display').append("<li class='input-1'>" + value + "</li>");
   });
 
-  
+  $('#button-2').click(function(){
+    var value = $('#field-2').val();
+    $('#display').append("<li class='input-1'>" + value + "</li>");
+  })
   
 });
